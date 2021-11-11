9119de1a3867848360bef5bad228bd8d6341ce69
galarist <galamboscristovao@gmail.com>
2021-11-11 16:58:40 +1000

    MAde a custom closing function for modal (override built in function)

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,17 @@
 var navBar = document.getElementsByClassName('navBar'); // get all elements
 Array.from(navBar).forEach((element, index) => {
-    element.innerHTML = 
-    `
+    element.innerHTML =
+        `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" data-bs-dismiss="modal">Cristovao Galambos</a>
+        <a onclick="closeModal()" class="navbar-brand">Cristovao Galambos</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-dismiss="modal">Home</a>
+                <li class="nav-item active">
+                    <a class="nav-link" accesskey="x" onclick="closeModal()">Home</a>
                 </li>
                 <li class="nav-item">
                     <a accesskey="q" class="nav-link" type="button" data-bs-toggle="modal" href="#aboutMe" role="button" 
@@ -36,3 +36,17 @@ Array.from(navBar).forEach((element, index) => {
     </nav>
     `
 });
+
+// Closing modal function
+function closeModal() {
+    document.body.removeAttribute('class');
+    document.body.removeAttribute('style');
+    const backdrop = document.querySelector('.modal-backdrop.show');
+    var modal = document.getElementsByClassName('modal');
+    for (var i = 0; i < modal.length; i++) {
+        // Make modal hide
+        modal[i].style.display = "none";
+        // Remove the `modal-open` class from the body
+        backdrop.remove();
+    }
+}
